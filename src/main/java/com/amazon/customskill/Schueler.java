2512581b7bf8a1a15e19c3f66affdaaf98bde50b
Joycelea <joyce_kandja@yahoo.com>
2019-01-21 21:44:53 +0100

    Update Schueler.java
    Ich habe die fragen gestellt.

--- src/main/java/com/amazon/customskill/Schueler.java
+++ src/main/java/com/amazon/customskill/Schueler.java
@@ -2,4 +2,25 @@ package com.amazon.customskill;
 
 public class Schueler {
 
+  private String selectQuestion(int i) {
+        String question;
+        switch(i){
+            case 0:
+                question ="Ist der schüler oder die schülerin aelter als zwölf?";
+                counter ++;
+                break;
+            case 1:
+                question = "Welche farbe bevorzügt er oder sie, weiß schwarz oder grau?";
+                counter ++;
+                break;
+        
+            default:
+                question = "Der Counter ist kleiner 0 oder größer 1!";
+                //counter = 0;
+                //The program doesn't stop at the moment rather it starts at question 1
+
+        }
+        return question;
+    }
+
 }
