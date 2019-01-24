2b268f412f98130f0f7d0828648a1ed4c8f34d3c
Joycelea <joyce_kandja@yahoo.com>
2019-01-24 12:17:29 +0100

    zweite korrektur Senioren.java

--- src/main/java/com/amazon/customskill/Senioren.java
+++ src/main/java/com/amazon/customskill/Senioren.java
@@ -1,11 +1,14 @@
 package com.amazon.customskill;
 
 public class Senioren {
-	
+	private String[][] laptopDataBase = new String[4][2];
 	String allAnswers = "";
 	Boolean noMoreQuestions = false;
 	int frageCounter = 0;
 	
+public Senioren(){
+	ListAusfuellen();
+}	
  private String selectQuestion() {
         String question;
         switch(frageCounter){
@@ -24,46 +27,54 @@ public class Senioren {
 
  
   private String takeAnswer(String antwort) {
-	 	  String answer0 = "";
-	  	  String answer1 = "";
-	  switch(frageCounter){
-      case 0:
-          if(antwort.equalsIgnoreCase("ja")) {
-        	  answer0 ="ja";
-          }else {
-        	  answer0 = "nein";
-          }
-          counter = counter + 1;
-          break;
-      case 1:
-    	  if(antwort.equalsIgnoreCase("ja")) {
-        	  answer1 = "ja";
-          }else {
-        	  answer1 = "nein";
-          }
-          break;
-   }
-	  allAnswers = answer0 + answer1;
-  return allAnswers;
+	  String answer0 = "";
+ 	  String answer1 = "";
+ switch(frageCounter){
+ case 0:
+     if(antwort.equalsIgnoreCase("ja")) {
+   	  answer0 ="<= 2kg";
+     }else {
+   	  answer0 = "> 2kg";
+     }
+     counter = counter + 1;
+     break;
+ case 1:
+	  if(antwort.equalsIgnoreCase("ja")) {
+   	  answer1 = "grosses bildschirm";
+     }else {
+   	  answer1 = "normales bildschirm";
+     }
+     break;
+}
+ allAnswers = answer0 + "," + answer1;
+return allAnswers;
+}
+  
+ public void ListAusfuellen() { // Diese Methode fuellt unsere Database. 
+  	
+  	//laptopDataBase[i][0] = "Gewicht, Bildschirm grosse" ;    //laptopDataBase[i][1] = "Notebook" ;
+  	
+  laptopDataBase[0][0] = "<= 2kg,grosses bildschirm"  ;     laptopDataBase[1][1] =  "..." ;
+  laptopDataBase[1][0] = "<= 2kg,normales bildschirm" ;     laptopDataBase[2][1] =  "..." ;
+  laptopDataBase[2][0] = "> 2kg,grosses bildschirm" ;		laptopDataBase[3][1] =  "..." ;
+  laptopDataBase[3][0] = "> 2kg,normales bildschirm ;	    laptopDataBase[4][1] =  "..." ;   // die Tabelle ist komplett ausgefuellt
   }
   
-  private String computerName() {
-	  String laptopName;
-	  String noInput = "";
-	  if(allAnswers.equals("jaja")) {
-		  laptopName = ""; //laptop name oder ausgabe
-		 }else if(allAnswers.equals("janein")) {
-		         laptopName = ""; //laptop name oder ausgabe
-		 }else if(allAnswers.equals("neinnein")) {
-			 laptopName = ""; //laptop name oder ausgabe
-		 }else if(allAnswers.equals("neinja")) {
-			 laptopName = ""; //laptop name oder ausgabe
-		 }else{
-		  laptopName = noInput;
-		  System.out.println("error");
-		 }
-	  return laptopName;
-  }
+
+  public String getLaptopFromAnswers() {
+   	
+   	String antwort = "";
+   	for (int i=1; i < laptopDataBase.length; i++) {
+   		if (laptopDataBase[i][0].equals(allAnswers)) {
+   			antwort = laptopDataBase[i][1];
+   			break;
+   		}
+   	}
+   	
+   	String aussage1= "Die "+antwort+ " ist für dich ein guter Auswahl. Außerdem empfehle ich dir auch eine Mause zu kaufen." ;
+   	return aussage1;
+   }
+  
 
 }
 
