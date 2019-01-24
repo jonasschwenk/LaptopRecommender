47bd9646e55fa25cc2c5b36d71a816beaf465c77
sjDominikT <44141903+sjDominikT@users.noreply.github.com>
2019-01-24 12:21:19 +0100

    Update Nutzer.java
    kleine Anpassungen am Interface

--- src/main/java/com/amazon/customskill/Nutzer.java
+++ src/main/java/com/amazon/customskill/Nutzer.java
@@ -2,21 +2,20 @@ package com.amazon.customskill;
 
 public interface Nutzer{
 	
-	//Informationen und Statuswerte f¸r Nutzergruppe
-	int counter = 0;    //"Z‰hlt" die Fragen, gibt an welche Frage als n‰chstes dran ist
-	int minimalPrice = 0; //Gibt mindestpreis f¸r Laptopkategorie an
-	int NumberOfQuestions = 0; //Gibt an, wie viele Fragen es gibt
-	int answers [] = {0}; //Array zum speichern der Nutzeranforderungen
+	//Informationen und Statuswerte f√ºr Nutzergruppe
+	int frageCounter = 0;    //"Z√§hlt" die Fragen, gibt an welche Frage als n√§chstes dran ist
+	int minimalPrice = 0; //Gibt mindestpreis f√ºr Laptopkategorie an
+	boolean noMoreQuestions = false;
 	
 	//getter und setter
 	public int getCounter();
-	public void setCounter(int counter);
-	public int getAnswers(int index);
-	public void setAnswers(int index, int value);
+	public void setFrageCounter(int frageCounter);
 	
 	//Methoden
-	public String selectQuestion();  //Ist daf¸r da die Frage auszuw‰hlen, intern ist Antwort von counter abh‰ngig
-	public String getLaptopFromAnswers(); //Wertet das Array "answers" aus, ausgabe ist intern von diesem abh‰ngig
+	public String selectQuestion();  //Ist daf√ºr da die Frage auszuw√§hlen, intern ist Antwort von counter abh√§ngig
+	public String getLaptopFromAnswers(); //Wertet das Array "answers" aus, ausgabe ist intern von diesem abh√§ngig
+	public void listAusfuellen();
+	public void takeAnswer(String answer);
 	
 
 	
