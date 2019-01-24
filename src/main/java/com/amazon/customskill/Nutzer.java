e2b26418c2d0bee7a8b09b57b580b8ed2bd8f086
sjDominikT <44141903+sjDominikT@users.noreply.github.com>
2019-01-24 16:55:05 +0100

    Update Nutzer.java
    getter für noMoreQuestions hinzugefügt

--- src/main/java/com/amazon/customskill/Nutzer.java
+++ src/main/java/com/amazon/customskill/Nutzer.java
@@ -1,22 +1,26 @@
 package com.amazon.customskill;
 
-public interface Nutzer{
-	
-	//Informationen und Statuswerte für Nutzergruppe
-	int frageCounter = 0;    //"Zählt" die Fragen, gibt an welche Frage als nächstes dran ist
-//	int minimalPrice = 0; //Gibt mindestpreis für Laptopkategorie an
-	boolean noMoreQuestions = false;
-	
-	//getter und setter
-	public int getFrageCounter();
-	public void setFrageCounter(int frageCounter);
-	
-	//Methoden
-	public String selectQuestion();  //Ist dafür da die Frage auszuwählen, intern ist Antwort von counter abhängig
-	public String getLaptopFromAnswers(); //Wertet das Array "answers" aus, ausgabe ist intern von diesem abhängig
-	public void listAusfuellen(); //Initialisiert die Datenbank	
-	public String takeAnswers(int answer); //Nimmt antwort entgegen
-	
+public interface Nutzer {
 
-	
+	// Informationen und Statuswerte für Nutzergruppe
+	int frageCounter = 0; // "Zählt" die Fragen, gibt an welche Frage als nächstes dran ist
+	int minimalPrice = 0; // Gibt mindestpreis für Laptopkategorie an
+	boolean noMoreQuestions = false;
+
+	// getter und setter
+	public int getCounter();
+
+	public void setFrageCounter(int frageCounter);
+
+	// Methoden
+	public String selectQuestion(); // Ist dafür da die Frage auszuwählen, intern ist Antwort von counter abhängig
+
+	public String getLaptopFromAnswers(); // Wertet das Array "answers" aus, ausgabe ist intern von diesem abhängig
+
+	public void listAusfuellen();
+
+	public void takeAnswer(int answerAsInt);
+
+	public boolean getNoMoreQuestions();
+
 }
